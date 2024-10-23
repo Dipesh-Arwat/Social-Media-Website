5951009b67f293971c09b581fb784488d76696e0
Dipesh Arwat <dipesharwat80@gmail.com>
2024-10-23 08:52:24 +0530

    Changes

--- insta-frontend/src/App.js
+++ insta-frontend/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
-import Feed from './Components/Feed/Feed';
-import Profile from './Components/Profile/Profile';
-import EditProfile from './Components/EditProfile/EditProfile';
-import UploadPost from './Components/UploadPost/UploadPost';
-import Search from './Components/Search/Search';
-import ProtectedRoute from './ProtectedRoute';
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
 import axios from './axios';
-import UserProfile from './Components/UserProfile/UserProfile';
+
+// Lazy load components
+const Login = React.lazy(() => import('./Components/Login/Login'));
+const Register = React.lazy(() => import('./Components/Register/Register'));
+const Feed = React.lazy(() => import('./Components/Feed/Feed'));
+const Profile = React.lazy(() => import('./Components/Profile/Profile'));
+const EditProfile = React.lazy(() => import('./Components/EditProfile/EditProfile'));
+const UploadPost = React.lazy(() => import('./Components/UploadPost/UploadPost'));
+const Search = React.lazy(() => import('./Components/Search/Search'));
+const UserProfile = React.lazy(() => import('./Components/UserProfile/UserProfile'));
+const ProtectedRoute = React.lazy(() => import('./ProtectedRoute'));
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -19,80 +21,71 @@ function App() {
   const [loading, setLoading] = useState(true); // Track the loading state for user fetching
 
   useEffect(() => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    setIsAuthenticated(true);
-    
-    // Fetch the current user's profile using the /currentUser route
-    const fetchCurrentUser = async () => {
-      try {
-        const res = await axios.get('/currentUser', {
-          headers: {
-            Authorization: `Bearer ${token}`, // Send the token in the Authorization header
-          },
-        });
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+      
+      // Fetch the current user's profile using the /currentUser route
+      const fetchCurrentUser = async () => {
+        try {
+          const res = await axios.get('/currentUser', {
+            headers: {
+              Authorization: `Bearer ${token}`, // Send the token in the Authorization header
+            },
+          });
 
-        localStorage.setItem('profileImage', res.data.profileImage || '');
-        setCurrentUser(res.data);
-      } catch (error) {
-        console.error('Error fetching current user:', error);
-        localStorage.removeItem('token');
-        setIsAuthenticated(false);
-      } finally {
-        setLoading(false);  // Update loading state
-      }
-    };
+          localStorage.setItem('profileImage', res.data.profileImage || '');
+          setCurrentUser(res.data);
+        } catch (error) {
+          console.error('Error fetching current user:', error);
+          localStorage.removeItem('token');
+          setIsAuthenticated(false);
+        } finally {
+          setLoading(false);  // Update loading state
+        }
+      };
 
-    fetchCurrentUser();
-  } else {
-    setIsAuthenticated(false);
-    setLoading(false);  // Update loading state
+      fetchCurrentUser();
+    } else {
+      setIsAuthenticated(false);
+      setLoading(false);  // Update loading state
+    }
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <div className="loading-spinner"></div>
+        <p>Loading, please wait...</p>
+      </div>
+    );
   }
-}, []);
-
-
-if (loading) {
-  return (
-    <div className="loading-container">
-      <div className="loading-spinner"></div>
-      <p>Loading, please wait...</p>
-    </div>
-  );
-}
-
 
   return (
     <Router>
       <div className="app">
         {isAuthenticated && <Sidebar setIsAuthenticated={setIsAuthenticated} />}
         <div className="content">
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-            <Route path="/register" element={<Register />} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" />} />
+              <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+              <Route path="/register" element={<Register />} />
 
-            {isAuthenticated ? (
-              <>
-                <Route path="/feed" element={<ProtectedRoute component={Feed} />} />
-                <Route path="/user-profile/:userId" element={<UserProfile />} />
-                {/* <Route path="/profile" element={<Navigate to={`/profile/${currentUser ? currentUser._id : ''}`} />} /> */}
-                {/* <Route path="/profile" element={<ProtectedRoute component={Profile} />} /> */}
-
-                <Route path="/profile" element={<ProtectedRoute component={Profile} currentUser={currentUser} />} />
-
-                
-
-                <Route
-                  path="/profile/edit"
-                  element={<ProtectedRoute component={EditProfile} currentUser={currentUser} />}
-                />
-                <Route path="/upload" element={<ProtectedRoute component={UploadPost} />} />
-                <Route path="/search" element={<ProtectedRoute component={Search} />} />
-              </>
-            ) : (
-              <Route path="*" element={<Navigate to="/login" />} />
-            )}
-          </Routes>
+              {isAuthenticated ? (
+                <>
+                  <Route path="/feed" element={<ProtectedRoute component={Feed} />} />
+                  <Route path="/user-profile/:userId" element={<UserProfile />} />
+                  <Route path="/profile" element={<ProtectedRoute component={Profile} currentUser={currentUser} />} />
+                  <Route path="/profile/edit" element={<ProtectedRoute component={EditProfile} currentUser={currentUser} />} />
+                  <Route path="/upload" element={<ProtectedRoute component={UploadPost} />} />
+                  <Route path="/search" element={<ProtectedRoute component={Search} />} />
+                </>
+              ) : (
+                <Route path="*" element={<Navigate to="/login" />} />
+              )}
+            </Routes>
+          </Suspense>
         </div>
       </div>
     </Router>
