08661b4ad1dc1b739bb64530855fc4385dc7937d
Dipesh Arwat <dipesharwat80@gmail.com>
2024-10-25 10:06:55 +0530

    changes

--- insta-frontend/src/axios.js
+++ insta-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000' || 'https://social-media-website-backend-0xnf.onrender.com/api', // Update this with your backend URL
+  baseURL: 'https://social-media-website-backend-0xnf.onrender.com/api', // Update this with your backend URL
 });
 
 export default instance;
