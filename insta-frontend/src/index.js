5602d143b842601ad2ff803adfa8b36e0710267c
Dipesh Arwat <dipesharwat80@gmail.com>
2024-10-27 16:48:54 +0530

    Changes

--- insta-frontend/src/index.js
+++ insta-frontend/src/index.js
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-
+  </React.StrictMode>
 );
 // if ('serviceWorker' in navigator) {
 //   window.addEventListener('load', () => {
