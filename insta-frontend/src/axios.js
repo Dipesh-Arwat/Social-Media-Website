e43352d8c29b8a976609a98ab96c79d541e299f4
Dipesh Arwat <dipesharwat80@gmail.com>
2024-11-24 07:06:11 +0530

    changes

--- insta-frontend/src/axios.js
+++ insta-frontend/src/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-  ? 'https://social-media-website-backend-0xnf.onrender.com/api' 
-  : 'http://localhost:5000/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
 export default instance;
